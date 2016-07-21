2f3b7512cb466d2e5502e3147b951cbfaa2f0f07
t-nagaya-t <t-nagaya@towatech.net>
2016-07-21 16:54:42 +0900

    2を渡すと2を返すテストが失敗する

--- test/lib/fizz_buzz_test.rb
+++ test/lib/fizz_buzz_test.rb
@@ -7,4 +7,9 @@ class FizzBuzzTest < Test::Unit::TestCase
     fizz_buzz = FizzBuzz.new
     assert_equal '1', fizz_buzz.convert(1)
   end
+
+  def test_2を渡すと2を返す
+    fizz_buzz = FizzBuzz.new
+    assert_equal '2', fizz_buzz.convert(2)
+  end
 end

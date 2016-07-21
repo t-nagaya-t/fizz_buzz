daea35360810b034c4b64d1da0889687da1bd93b
t-nagaya-t <t-nagaya@towatech.net>
2016-07-21 18:09:20 +0900

    15の倍数に対してFizzBuzzを返す

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,17 @@
 class FizzBuzz
 
+  class << self
+    def print_fizz_buzz(number)
+      1.upto(number.to_i) do |i|
+        puts self.new.convert(i)
+      end
+    end
+  end
+
   def convert(number)
-    if number % 3 == 0
+    if number % 15 == 0
+      'FizzBuzz'
+    elsif number % 3 == 0
       'Fizz'
     elsif number % 5 == 0
       'Buzz'
--- test/lib/fizz_buzz_test.rb
+++ test/lib/fizz_buzz_test.rb
@@ -7,6 +7,18 @@ class FizzBuzzTest < Test::Unit::TestCase
     @fizz_buzz = FizzBuzz.new
   end
 
+  sub_test_case 'FizzBuzz問題' do
+    def test_1から100まで表示する
+      FizzBuzz.print_fizz_buzz(100)
+    end
+  end
+
+  sub_test_case '3かつ5の倍数の時は数の代わりにFizzBuzzと返す' do
+    def test_15の場合
+      assert_equal 'FizzBuzz', @fizz_buzz.convert(15)
+    end
+  end
+
   sub_test_case '3の倍数の時は数の代わりにFizzと返す' do
     def test_3の場合
       assert_equal 'Fizz', @fizz_buzz.convert(3)
